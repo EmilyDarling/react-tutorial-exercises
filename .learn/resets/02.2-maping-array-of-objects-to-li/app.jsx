af578c4a0248c022f8d90c63c0edec65aa232017
Angelina <angelaaquin13@gmail.com>
2024-08-27 01:02:13 +0000

    initial

--- .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
+++ .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
@@ -7,15 +7,10 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * 1) Create the mapping function and use it to generate a new array of
  * planets in html called planetsInHTML
  */
-const planetMap = planets.map((planet, index) => {
-	return (
-		<li class="list-group-item" key={index}>
-			{planet}
-		</li>
-	);
-});
+
+
 
 // 2) Add the array planetsInHTML inside the innerHTML of this <ul>
-const content = <ul className="list-group m-5">{planetMap}</ul>;
+const content = (<ul className="list-group m-5"></ul>);
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
--- exercises/01-hello-world/app.jsx
+++ exercises/01-hello-world/app.jsx
@@ -1,12 +1,18 @@
 import React from "react"; // Main React.js library
 
-import ReactDOM from "react-dom"; // We use ReactDOM to render into the DOM
+//import ReactDOM from "react-dom";  We use ReactDOM to render into the DOM
+import { createRoot } from "react-dom/client";
 
 // WHAT: This variable contains the html to render
-let output = <span>James is 12 years old</span>;
-
+let output = (
+	<span>
+		James is <strong>12</strong> years old
+	</span>
+);
+//------this has been updated to a newer version of react------------
 // WHERE: A DOM element that will contain the entire react generated html
-const myDiv = document.querySelector("#myDiv");
+const domNode = document.getElementById("myDiv");
+const root = createRoot(domNode);
 
-                //what  //where
-ReactDOM.render(output, myDiv);
+//what  //where
+root.render(output);
